e9ad1b431c4d1bf4440f70bb4f3c2fd45b7df2d7
Nikolai Kuryshev <klaus1776@hotmail.com>
2025-01-29 15:43:10 +0500

    Добавил комментарии для того, чтобы создать PullRequest в Git-е

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
+        // Создание экземпляра Student-а:
         Student student1 = new Student("МГУ", "Росиия", "Петров Петр Петрович", "119991, Российская Федерация, Москва, Ленинские горы, д.1", 18, 3,4,3,5,5,5);
+        // Рефлексия:
         TestRunner.setObject(student1);
         TestRunner.runTests(student1.getClass());
       }
